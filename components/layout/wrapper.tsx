7c1957c5546f5a64927c5559f7de32bbd55daa51
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-07 18:59:27 -0400

    feat: include search box in wrapper

--- components/layout/wrapper.tsx
+++ components/layout/wrapper.tsx
@@ -1,7 +1,9 @@
+import type { ComponentChildren } from 'preact'
 import Header from 'components/layout/header.tsx'
 import Footer from 'components/layout/footer.tsx'
 import SubHeader from 'components/layout/sub-header.tsx'
-import type { ComponentChildren } from 'preact'
+import SearchBox from 'islands/search-box.tsx'
+import { AiOutlineSearch } from 'react-icons/ai/mod.ts'
 
 interface Props {
   children: ComponentChildren
@@ -12,6 +14,14 @@ const Wrapper = ({ children }: Props) => (
     <Header />
     <main class='p-5 max-w-4xl mx-auto'>
       <SubHeader />
+      <div class='relative mt-5'>
+        <AiOutlineSearch
+          size={24}
+          class='z-10 text-gray-500 font-semibold absolute top-1/2 left-3 -translate-y-1/2'
+          aria-hidden={true}
+        />
+        <SearchBox />
+      </div>
       {children}
     </main>
     <Footer />
