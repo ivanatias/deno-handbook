bec42aa485dbbc7d87e739bf4843e20d961b9b01
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-07 18:57:48 -0400

    feat: add article card

---
+++ components/article-card.tsx
@@ -0,0 +1,28 @@
+interface Props {
+  title: string
+  desc: string
+  href: string
+}
+
+const ArticleCard = ({ title, desc, href }: Props) => (
+  <a
+    class='hover:bg-green-50 border(& gray-200) p-4 rounded-lg transition duration-150 ease-in min-h-[200px]'
+    href={href}
+  >
+    <div class='flex(& col) gap-4 max-w-prose'>
+      <h3 class='text(base lg:lg green-400) font-semibold mb-2 underline'>
+        {title}
+      </h3>
+      <p class='text(xs lg:sm black) font-light'>
+        {desc}
+      </p>
+      <div class='flex justify-end'>
+        <span class='text(xs green-400) font-semibold mt-3'>
+          Leer más...
+        </span>
+      </div>
+    </div>
+  </a>
+)
+
+export default ArticleCard
