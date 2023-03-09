f742a018cc9082932cf00772ad295e4d924fe820
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-08 21:21:56 -0400

    styles(sub-header): center title on mobile

--- components/layout/sub-header.tsx
+++ components/layout/sub-header.tsx
@@ -1,7 +1,7 @@
 const SubHeader = () => (
   <div class='flex(& col lg:row) gap-3 lg:justify-between items-center'>
     <div class='flex(& col) gap-2 text(black dark:gray-300)'>
-      <h1 class='text(3xl lg:5xl green-400) font-bold mb-3'>
+      <h1 class='text(3xl center lg:left lg:5xl green-400) font-bold mb-3'>
         ¡Aprende acerca de Deno!
       </h1>
       <h2 class='text(base center lg:left lg:xl) font-semibold'>
