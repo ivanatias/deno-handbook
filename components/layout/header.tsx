c87bfcd6c6f4f5013951ac87a678204293e3d0df
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-08 21:20:41 -0400

    refactor: modify header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,22 +1,16 @@
 import { AiFillGithub } from 'react-icons/ai/mod.ts'
 
 const Header = () => (
-  <header class='p-5 max-w-4xl mx-auto text(black dark:white)'>
-    <div class='w-full flex gap-2 items-center justify-end'>
-      <a
-        href='https://github.com/ivanatias/deno-handbook'
-        target='_blank'
-        rel='noreferrer noopener'
-        aria-label='Project repository link opens a new window'
-        class='hover:scale-105 transition-transform duration-150 ease-in'
-      >
-        <AiFillGithub size={20} />
-      </a>
-      {/* Abstract to an island as this data will come from localStorage */}
-      <span class='text(xs 2xl:sm) underline'>
-        Has leído 1/100 artículos
-      </span>
-    </div>
+  <header class='flex justify-end p-5 max-w-4xl mx-auto text(black dark:white)'>
+    <a
+      href='https://github.com/ivanatias/deno-handbook'
+      target='_blank'
+      rel='noreferrer noopener'
+      aria-label='Project repository link opens a new window'
+      class='hover:scale-105 transition-transform duration-150 ease-in'
+    >
+      <AiFillGithub size={24} />
+    </a>
   </header>
 )
 
