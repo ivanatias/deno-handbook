8fe2eddda6c8e6e49baae3d3f6ebc99b127af91e
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-08 20:25:24 -0400

    feat: add article page

---
+++ routes/[slug].tsx
@@ -0,0 +1,84 @@
+import { Handlers, PageProps } from '$fresh/server.ts'
+import { Head } from '$fresh/runtime.ts'
+import Wrapper from 'components/layout/wrapper.tsx'
+import { getArticle } from 'utils/articles.ts'
+import { CSS } from 'gfm'
+import type { Article } from 'types'
+
+export const handler: Handlers<Article> = {
+  async GET(_req, ctx) {
+    const { slug } = ctx.params
+    const article = await getArticle(slug)
+
+    return article !== null ? ctx.render(article) : ctx.renderNotFound()
+  },
+}
+
+const Home = ({ data: article }: PageProps<Article>) => {
+  const { title, excerpt, date, author, authorUrl, body } = article
+
+  const hasAuthor = author !== undefined
+  const hasAuthorUrl = authorUrl !== undefined
+
+  return (
+    <>
+      <Head>
+        <title>Deno handbook en Español - {title}</title>
+        <meta name='description' content={excerpt} />
+        <style dangerouslySetInnerHTML={{ __html: CSS }} />
+      </Head>
+
+      <Wrapper>
+        <section class='mt-10 text(black dark:gray-300)'>
+          <article>
+            <header class='flex(& col) gap-3'>
+              <h2 class='text(2xl lg:3xl) font-bold'>
+                {title}
+              </h2>
+              {hasAuthor && (
+                <div class='flex items-center gap-1'>
+                  <span class='font-semibold'>
+                    Autor:
+                  </span>
+                  {hasAuthorUrl
+                    ? (
+                      <a
+                        href={authorUrl}
+                        target='_blank'
+                        rel='noreferrer noopener'
+                        class='hover:underline transition duration-150'
+                      >
+                        {author}
+                      </a>
+                    )
+                    : (
+                      <span>
+                        {author}
+                      </span>
+                    )}
+                </div>
+              )}
+              <div class='flex items-center gap-1'>
+                <span class='font-semibold'>
+                  Fecha de publicación:
+                </span>
+                <time>
+                  {Intl.DateTimeFormat('es').format(date)}
+                </time>
+              </div>
+            </header>
+            <div
+              data-color-mode='auto'
+              data-light-theme='light'
+              data-dark-theme='dark'
+              class='markdown-body mt-5'
+              dangerouslySetInnerHTML={{ __html: body }}
+            />
+          </article>
+        </section>
+      </Wrapper>
+    </>
+  )
+}
+
+export default Home
