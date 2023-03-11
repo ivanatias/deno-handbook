433cc337f6019c9f9bdec70855bb62ac7558efad
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-11 15:56:39 -0400

    feat: add new types

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,5 @@
+import type { JSX } from 'preact'
+
 interface Article {
   title: string
   date: Date
@@ -8,4 +10,23 @@ interface Article {
   body: string
 }
 
-export { Article }
+type SearchArticle = Pick<Article, 'title' | 'slug' | 'author'>
+
+type SearchMatch = {
+  indices: [number, number][]
+  key: string
+  value: string
+}
+
+interface SearchResult {
+  item: SearchArticle
+  matches: SearchMatch[]
+  refIndex: number
+  score: number
+}
+
+type Input = JSX.HTMLAttributes<HTMLInputElement>
+
+type InputChangeEvent = JSX.TargetedEvent<HTMLInputElement, Event>
+
+export { Article, Input, InputChangeEvent, SearchResult }
