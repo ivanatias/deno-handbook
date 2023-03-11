ac75efd92d6ac1136e9f84e70ad9a0240fb8991b
Ivan Atias <ivan.d.atias@gmail.com>
2023-03-11 17:25:54 -0400

    styles(sub-header): fix go back link styling

--- components/layout/sub-header.tsx
+++ components/layout/sub-header.tsx
@@ -18,7 +18,7 @@ const SubHeader = ({ isHome }: Props) => (
       {!isHome && (
         <a
           href='/'
-          class='text(xs center lg:base lg:left) mt-5 w-max font-bold hover:underline transition duration-150'
+          class='text(xs center lg:base lg:left) mt-5 w(full lg:max) font-bold hover:underline transition duration-150'
         >
           ← Volver al inicio
         </a>
